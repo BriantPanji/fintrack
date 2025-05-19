1f40089ae75294e149d8de3f7ea383ff2574bb9e
BriantPanji <panjibriant@students.usu.ac.id>
2025-05-19 22:48:20 +0700

    salah tahun kocak

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="w-full min-h-50 h-fit bg-ft-bg flex flex-col justify-center items-center px-4 py-2 gap-2">
             <h1 className="text-lg font-bold text-ft-primary">Made with <i className="fa-solid fa-heart text-red-600"></i> by Panji Depari</h1>
-            <p className="text-sm text-ft-secondary">© 2023 <a href="//github.com/BriantPanji" target="_blank">Panji Depari</a>. All rights reserved.</p>
+            <p className="text-sm text-ft-secondary">© 2025 <a href="//github.com/BriantPanji" target="_blank">Panji Depari</a>. All rights reserved.</p>
         </footer>
     )
 }
