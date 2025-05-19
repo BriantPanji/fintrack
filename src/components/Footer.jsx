a754ce318ef8380ea455516b955247127995b9b2
BriantPanji <panjibriant@students.usu.ac.id>
2025-05-19 22:49:58 +0700

    gabut banh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@
 export default function Footer() {
     return (
         <footer className="w-full min-h-50 h-fit bg-ft-bg flex flex-col justify-center items-center px-4 py-2 gap-2">
-            <h1 className="text-lg font-bold text-ft-primary">Made with <i className="fa-solid fa-heart text-red-600"></i> by Panji Depari</h1>
+            <h1 className="text-lg font-bold text-ft-primary">Made with <i className="fa-solid fa-heart text-red-600"></i> by <a href="//instagram.com/panjidepari" target="_blank">Panji Depari</a></h1>
             <p className="text-sm text-ft-secondary">© 2025 <a href="//github.com/BriantPanji" target="_blank">Panji Depari</a>. All rights reserved.</p>
         </footer>
     )
